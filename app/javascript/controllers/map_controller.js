affef458a91ffae80480199e752d6d59ee0e342b
AdrienLobry <adrien_lobry@live.fr>
2025-03-18 11:46:14 +0100

    mapbow marker custom

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,6 +1,6 @@
 // app/javascript/controllers/map_controller.js
 import { Controller } from "@hotwired/stimulus"
-import mapboxgl from 'mapbox-gl' // Don't forget this!
+import mapboxgl from "mapbox-gl"
 
 export default class extends Controller {
   static values = {
@@ -9,31 +9,52 @@ export default class extends Controller {
   }
 
   connect() {
+    // Set Mapbox access token
     mapboxgl.accessToken = this.apiKeyValue
 
+    // Initialize the Mapbox map
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/frenchdrinker/cm8e8x3r7000801sd1i0j80pq"
     })
+
+    // Add custom markers to the map
     this.#addMarkersToMap()
+
+    // Adjust the map view to fit all markers
     this.#fitMapToMarkers()
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      marker = JSON.parse(marker)
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
+      marker = typeof marker === "string" ? JSON.parse(marker) : marker
+
+      // Create the marker container
+      const markerContainer = document.createElement("div")
+      markerContainer.className = "custom-marker" // Apply CSS styles
+
+      // Create the image inside the marker
+      const markerImage = document.createElement("div")
+      markerImage.className = "marker-image"
+      markerContainer.appendChild(markerImage)
+
+      // Create and add the custom marker to the map
+      new mapboxgl.Marker(markerContainer)
+        .setLngLat([marker.lng, marker.lat])
         .addTo(this.map)
     })
   }
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
+
     this.markersValue.forEach((marker) => {
-      marker = JSON.parse(marker)
-      bounds.extend([ marker.lng, marker.lat ])
+      marker = typeof marker === "string" ? JSON.parse(marker) : marker
+      bounds.extend([marker.lng, marker.lat])
     })
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+
+    if (this.markersValue.length > 0) {
+      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    }
   }
 }
